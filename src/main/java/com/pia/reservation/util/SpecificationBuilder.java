c0dfd87cbeac6cc6735c7dcae78cce909f683f6f
erincak <erincak2@gmail.com>
2024-07-09 12:43:36 +0300

    filter updated without Date,and rooms structure has been changed succesfully,posting hotel with identical rooms

--- src/main/java/com/pia/reservation/util/SpecificationBuilder.java
+++ src/main/java/com/pia/reservation/util/SpecificationBuilder.java
@@ -3,6 +3,7 @@ package com.pia.reservation.util;
 import com.pia.reservation.model.Hotel;
 import com.pia.reservation.model.Location;
 import com.pia.reservation.model.Reservation;
+import com.pia.reservation.model.Room;
 import jakarta.persistence.criteria.*;
 import org.springframework.data.jpa.domain.Specification;
 
@@ -29,31 +30,57 @@ public class SpecificationBuilder<T> {
                     case "country":
                         Join<Hotel, Location> locationJoin = root.join("location");
                         predicates.add(criteriaBuilder.equal(locationJoin.get(key), value));
-                        Join<Hotel, Reservation> reservationJoin = root.join("reservation");
 
 
                         break;
                     case "amentities":
                         String[] amenitiesArray = value.split(",");
                         for (String amenity : amenitiesArray) {
-                            predicates.add(criteriaBuilder.like(root.get("amenities"), "%" + amenity.trim() + "%"));
+                            predicates.add(criteriaBuilder.like(root.get("amentities"), "%" + amenity.trim() + "%"));
                         }
                         break;
                     case "hotelType": predicates.add(criteriaBuilder.equal(root.get("hotelType"), value));
                     break;
+                    case "roomAmentites":
+                        Join<Hotel, Room> roomJoin = root.join("rooms");
+                        String[] roomAmenitiesArray = value.split(",");
+                        for(String roomAmenity : roomAmenitiesArray){
+                            System.out.println("Filtering for room amenity: " + roomAmenity);
+                            System.out.println(roomAmenity.trim());
+                            predicates.add(criteriaBuilder.like(roomJoin.get("roomAmentites"), "%" + roomAmenity.trim() + "%"));
+                        }
+                        //predicates.add(criteriaBuilder.equal(roomJoin.get(key), value));
+                        break;
+                    case "roomType":
+                        Join<Hotel, Room> roomTypeJoin = root.join("roomType");
+                        predicates.add(criteriaBuilder.equal(roomTypeJoin.get(key), value));
+                    break;
+
+                    case "maxOccupancy":
+                        Join<Hotel, Room> maxOccupancyJoin = root.join("maxOccupancy");
+                        predicates.add(criteriaBuilder.equal(maxOccupancyJoin.get(key), value));
+                        break;
+                    case "bedNo":
+                        Join<Hotel, Room> bedNoJoin = root.join("bedNo");
+                        predicates.add(criteriaBuilder.equal(bedNoJoin.get(key), value));
+                        break;
 
                 }
             }
             if(params.containsKey("sortBy")){
                 String sortBy = params.get("sortBy");
                 if("avg_score_asc".equalsIgnoreCase(sortBy)){
-                    query.orderBy(criteriaBuilder.asc(root.get("avg_score_asc")));
+                    query.orderBy(criteriaBuilder.asc(root.get("avgScore")));
                 }
-                else  if ("avg_score_desc".equalsIgnoreCase(sortBy)){}
-                query.orderBy(criteriaBuilder.desc(root.get("avg_score_asc")));
-            }
+                else  if ("avg_score_desc".equalsIgnoreCase(sortBy)){
+                    query.orderBy(criteriaBuilder.desc(root.get("avgScore")));
+                }
 
-            return criteriaBuilder.and(predicates.toArray(new Predicate[predicates.size()]));
+            }
+            Predicate finalPredicate = criteriaBuilder.and(predicates.toArray(new Predicate[predicates.size()]));
+            System.out.println("Final predicate: " + finalPredicate.toString());
+
+            return finalPredicate;
         };
     }
 }
